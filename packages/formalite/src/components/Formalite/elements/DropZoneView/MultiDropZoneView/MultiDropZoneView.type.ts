db1fb60d1848a29f200daa1c96a3a3cf39363c2d
Mostafa Roosta <mostafaroosta74@gmail.com>
2022-10-04 16:55:25 +0330

    docs: fix hint docs for DropZones

--- packages/formalite/src/components/Formalite/elements/DropZoneView/MultiDropZoneView/MultiDropZoneView.type.ts
+++ packages/formalite/src/components/Formalite/elements/DropZoneView/MultiDropZoneView/MultiDropZoneView.type.ts
@@ -17,17 +17,23 @@ export interface MultiDropZoneViewType extends BaseViewType {
   /**
    * Props that contain
    *
-   * @prop onChange --> A callback that gives value in arg
+   * @prop onChange > Callback will call when input value change (array)
    *
-   * @prop label
+   * @prop label > Label text for input
    *
-   * @prop helperText --> A string or ReactNode
+   * @prop helperText > Text show below component (ReactNode)
    *
-   * @prop dropZoneOptions --> Specify props that passed to react-dropzone Dropzone component
-   * @see [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
+   * @prop dropZoneOptions > Specify props that passed to react-dropzone Dropzone component [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
+   * @see [MultiDropZoneView docs](https://formalite-docs.novin.dev/?path=/docs/components-dropzoneview--multi-drop-zone-view)
    */
   inputProps: {
+    /**
+     * Callback will call when input value change (array)
+     */
     onChange?: (value: string) => void;
+    /*
+     * Label text for input
+     */
     label: ReactNode;
 
     /**
@@ -35,15 +41,19 @@ export interface MultiDropZoneViewType extends BaseViewType {
      * @see [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
      */
     dropZoneOptions?: Partial<DropzoneOptions>;
+    /**
+     * Text show below component (ReactNode)
+     */
     helperText?: ReactNode;
   };
 
   /**
-   * @function A callback function that runs when Upload occurred in MultiDropZoneView
+   * A callback function that runs when Upload occurred in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param file The choosen file
-   * @param progress A callback that gives progress number in arg
+   * @param file > The chosen file
+   * @param progress > A callback that gives progress number in arg
+   * @param uploadController > uploadController to connect to axios for auto abort
    */
   onUpload: (
     file: File,
@@ -52,11 +62,12 @@ export interface MultiDropZoneViewType extends BaseViewType {
   ) => Promise<string | undefined>;
 
   /**
-   * @function A callback function that runs when Delete occurred in MultiDropZoneView
+   * A callback function that runs when Delete occurred in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param id id of selected item to be deleted
-   * @param isFromDefault Tells you that this is a new item or it's from the default value
+   * @param id > id of selected item to be deleted
+   * @param isFromDefault > Tells you that this is a new item or it's from the default value
+   * @param isSuccess > Tells you delete was Success
    */
   onDelete: (
     id: string,
@@ -65,11 +76,11 @@ export interface MultiDropZoneViewType extends BaseViewType {
   ) => Promise<void>;
 
   /**
-   * @function A callback function that must run when an image want to be downloded in MultiDropZoneView
+   * A callback function that must run when an image want to be downloaded in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param pathUrl The url of downloaded image
-   * @param controller A controller object that allows you to abort one or more DOM requests as and when desired.
+   * @param pathUrl > The url of downloaded image
+   * @param controller > A controller object that allows you to abort one or more DOM requests as and when desired.
    */
   imageDownloader?: (
     pathUrl: string,
--- packages/formalite/src/components/Formalite/elements/DropZoneView/SingleDropZoneView/SingleDropZoneView.type.ts
+++ packages/formalite/src/components/Formalite/elements/DropZoneView/SingleDropZoneView/SingleDropZoneView.type.ts
@@ -14,17 +14,23 @@ export interface SingleDropZoneViewType extends BaseViewType {
   /**
    * Props that contain
    *
-   * @prop onChange --> A callback that gives value in arg
+   * @prop onChange > Callback will call when input value change (array)
    *
-   * @prop label
+   * @prop label > Label text for input
    *
-   * @prop helperText --> A string or ReactNode
+   * @prop helperText > Text show below component (ReactNode)
    *
-   * @prop dropZoneOptions --> Specify props that passed to react-dropzone Dropzone component
-   * @see [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
+   * @prop dropZoneOptions > Specify props that passed to react-dropzone Dropzone component [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
+   * @see [SingleDropZoneView docs](https://formalite-docs.novin.dev/?path=/docs/components-dropzoneview--single-drop-zone-view)
    */
   inputProps: {
+    /**
+     * Callback will call when input value change (array)
+     */
     onChange?: (value: string) => void;
+    /*
+     * Label text for input
+     */
     label: ReactNode;
 
     /**
@@ -32,15 +38,19 @@ export interface SingleDropZoneViewType extends BaseViewType {
      * @see [react-dropzone Dropzone](https://react-dropzone.js.org/#src)
      */
     dropZoneOptions?: Partial<DropzoneOptions>;
+    /**
+     * Text show below component (ReactNode)
+     */
     helperText?: ReactNode;
   };
 
   /**
-   * @function A callback function that runs when Upload occurred in MultiDropZoneView
+   * A callback function that runs when Upload occurred in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param file The choosen file
-   * @param progress A callback that gives progress number in arg
+   * @param file > The chosen file
+   * @param progress > A callback that gives progress number in arg
+   * @param uploadController > uploadController to connect to axios for auto abort
    */
   onUpload: (
     file: CustomFile,
@@ -49,11 +59,12 @@ export interface SingleDropZoneViewType extends BaseViewType {
   ) => Promise<string>;
 
   /**
-   * @function A callback function that runs when Delete occurred in MultiDropZoneView
+   * A callback function that runs when Delete occurred in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param id id of selected item to be deleted
-   * @param isFromDefault Tells you that this is a new item or it's from the default value
+   * @param id > id of selected item to be deleted
+   * @param isFromDefault > Tells you that this is a new item or it's from the default value
+   * @param isSuccess > Tells you delete was Success
    */
   onDelete: (
     id: string,
@@ -62,11 +73,11 @@ export interface SingleDropZoneViewType extends BaseViewType {
   ) => Promise<void>;
 
   /**
-   * @function A callback function that must run when an image want to be downloded in MultiDropZoneView
+   * A callback function that must run when an image want to be downloaded in MultiDropZoneView
    *
    * As an args of callback ,gives you :
-   * @param pathUrl The url of downloaded image
-   * @param controller A controller object that allows you to abort one or more DOM requests as and when desired.
+   * @param pathUrl > The url of downloaded image
+   * @param controller > A controller object that allows you to abort one or more DOM requests as and when desired.
    */
   imageDownloader?: (
     pathUrl: string,

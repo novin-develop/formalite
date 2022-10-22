a0f2e865b7cb2f4dc7a498675fad3a04665d54a6
Mostafa Roosta <106796626+mostafaRoosta74@users.noreply.github.com>
2022-10-22 15:40:48 +0330

    docs: fix next.config.js

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const removeImports = require("next-remove-imports")();
+module.exports = removeImports({
   reactStrictMode: true,
   swcMinify: false,
-}
-
-module.exports = nextConfig
+  experimental: { esmExternals: true }
+});

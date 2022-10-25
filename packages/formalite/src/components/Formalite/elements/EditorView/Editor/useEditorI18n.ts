5e335c9d1e69657d90b02a10b11a56c734ca9d48
Mostafa Roosta <mostafaroosta74@gmail.com>
2022-10-25 17:14:12 +0330

    refactor: refactor editText to new Component

---
+++ packages/formalite/src/components/Formalite/elements/EditorView/Editor/useEditorI18n.ts
@@ -0,0 +1,110 @@
+export const useEditorI18n = () => {
+  return {
+    common: {
+      ok: "OK",
+      delete: "Delete",
+      enter: "Enter",
+    },
+
+    blockQuote: {
+      title: "Quote",
+    },
+    codeBlock: {
+      title: "Code block",
+    },
+    color: {
+      color: "Font color",
+      bgColor: "Back color",
+      default: "Default color",
+      clear: "Clear back color",
+    },
+    divider: {
+      title: "Divider",
+    },
+    emotion: {
+      title: "Emotion",
+    },
+    fontSize: {
+      title: "Font size",
+      default: "Default",
+    },
+    fontFamily: {
+      title: "Font family",
+      default: "Default",
+    },
+    fullScreen: {
+      title: "Full screen",
+    },
+    header: {
+      title: "Header",
+      text: "Text",
+    },
+    image: {
+      netImage: "Net image",
+      delete: "Delete image",
+      edit: "Edit image",
+      viewLink: "View link",
+      src: "Image src",
+      desc: "Description",
+      link: "Image link",
+    },
+    indent: {
+      decrease: "Decrease",
+      increase: "Increase",
+    },
+    justify: {
+      left: "Left",
+      right: "Right",
+      center: "Center",
+      justify: "Justify",
+    },
+    lineHeight: {
+      title: "Line height",
+      default: "Default",
+    },
+    link: {
+      insert: "Insert link",
+      text: "Link text",
+      url: "Link source",
+      unLink: "Unlink",
+      edit: "Edit link",
+      view: "View link",
+    },
+    textStyle: {
+      bold: "Bold",
+      clear: "Clear styles",
+      code: "Inline code",
+      italic: "Italic",
+      sub: "Sub",
+      sup: "Sup",
+      through: "Through",
+      underline: "Underline",
+    },
+    undo: {
+      undo: "undo",
+      redo: "Redo",
+    },
+    todo: {
+      todo: "Todo",
+    },
+    uploadImgModule: {
+      uploadImage: "Upload image",
+    },
+    tableModule: {
+      insertTable: "Insert Table",
+    },
+    videoModule: {
+      insertVideo: "Insert Video",
+      uploadVideo: "Upload Video",
+      videoSrc: "Video source",
+      videoSrcPlaceHolder: "Video source address",
+      videoPoster: "Video poster",
+      videoPosterPlaceHolder: "Video poster address",
+      ok: "ok",
+    },
+    listModule: {
+      unOrderedList: "Un ordered list",
+      orderedList: "Ordered list",
+    },
+  };
+};
--- packages/formalite/src/components/Formalite/elements/EditorView/EditorView.tsx
+++ packages/formalite/src/components/Formalite/elements/EditorView/EditorView.tsx
@@ -1,26 +1,27 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { FormikProps, FormikValues } from "formik";
 import { OptionalObjectSchema } from "yup/lib/object";
-import ReactQuill from "react-quill";
-import { FormHelperText, Grid, InputLabel } from "@mui/material";
+import { FormHelperText, Grid, InputLabel, useTheme } from "@mui/material";
 import {
   checkIsRequired,
   getData,
   handleRandomClassNameOrId,
 } from "@components/Formalite/config/utils";
-import { Theme } from "@mui/material/styles/createTheme";
-import { Global } from "@emotion/react";
+import { css, Global } from "@emotion/react";
+import { useEditorI18n } from "@components/Formalite/elements/EditorView/Editor/useEditorI18n";
+import { IDomEditor, IEditorConfig, IToolbarConfig } from "@wangeditor/editor";
+import { CustomFile } from "@components/Formalite";
 import { baseMemo } from "../Bases/functions/memo";
 import type { EditorViewType } from "./EditorView.type";
 import { TextViewSkeleton } from "../Bases/SkeletonBase";
-import EditorViewRootStyle from "./EditorViewStyle";
-import EditorToolbar, {
-  formats,
-  redoChange,
-  undoChange,
-} from "./Editor/EditorToolbar";
 import { cssText } from "./editorCss";
 
+/**
+ * TODO add I18n
+ * TODO remove and organize
+ * TODO fix spreading Pops override
+ * TODO fix and check upload image
+ */
 interface EditorViewProps<T> {
   allData: EditorViewType;
   name: string;
@@ -29,37 +30,137 @@ interface EditorViewProps<T> {
   validationSchema: OptionalObjectSchema<any>;
   translator: Function;
 }
+
+let Editor = (...props: any) => <>a</>;
+let Toolbar = (...props: any) => <>b</>;
+let i18nChangeLanguage = (a: string) => {};
+let i18nAddResources = (a: string, b: Object) => {};
+
 const EditorView = <T extends FormikValues>(props: EditorViewProps<T>) => {
   const { allData, name, formik, loading, validationSchema, translator } =
     props;
-  const { helperText, label, ...editorProps } = allData.editorProps;
+  const {
+    helperText,
+    label,
+    editorConfig: editorConfigProps = {},
+    toolbarConfig: toolbarConfigProps = {},
+    toolbarComponentProps = {},
+    editorComponentProps = {},
+  } = allData.editorProps;
+  const [editor, setEditor] = useState<IDomEditor | null>(null);
+  const [load, setLoad] = useState(false);
+  const i18nObject = useEditorI18n();
+  const theme = useTheme();
+  const boxRef = useRef<HTMLDivElement>(null);
 
-  const id = useMemo(() => `minimal-quill-${handleRandomClassNameOrId()}`, []);
+  useEffect(() => {
+    // eslint-disable-next-line global-require
+    const editorReact = require("@wangeditor/editor-for-react");
+    // eslint-disable-next-line global-require
+    const newEditor = require("@wangeditor/editor");
+    Editor = editorReact.Editor;
+    Toolbar = editorReact.Toolbar;
+    i18nChangeLanguage = newEditor.i18nChangeLanguage;
+    i18nAddResources = newEditor.i18nAddResources;
 
-  const modules = useMemo(
-    () => ({
-      toolbar: {
-        container: `#${id}`,
-        handlers: {
-          undo: undoChange,
-          redo: redoChange,
-          // image: quillImageHandler,
+    i18nAddResources("new", i18nObject);
+    i18nChangeLanguage("new");
+
+    if (document) {
+      setLoad(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const r = document.querySelector<any>(":root")!;
+    if (theme.palette.mode === "dark") {
+      if (boxRef.current) {
+        boxRef.current.style.border = "1px solid #ffffff3b";
+      }
+      // r.style.setProperty("--w-e-textarea-bg-color", "#333");
+      r.style.setProperty("--w-e-textarea-color", "#fff");
+      r.style.setProperty("--w-e-toolbar-bg-color", "#333");
+      r.style.setProperty("--w-e-toolbar-color", "#fff");
+      r.style.setProperty(
+        "--w-e-toolbar-active-bg-color",
+        theme.palette.grey[700]
+      );
+      r.style.setProperty(
+        "--w-e-toolbar-active-color",
+        theme.palette.grey[500]
+      );
+      r.style.setProperty("--w-e-toolbar-active-color_options", "#fff");
+    } else {
+      if (boxRef.current) {
+        boxRef.current.style.border = "1px solid #0000003b";
+      }
+      // r.style.setProperty("--w-e-textarea-bg-color", "#fff");
+      r.style.setProperty("--w-e-textarea-color", "#333");
+      r.style.setProperty("--w-e-toolbar-bg-color", "#fff");
+      r.style.setProperty("--w-e-toolbar-color", "#333");
+      r.style.setProperty(
+        "--w-e-toolbar-active-bg-color",
+        theme.palette.grey[200]
+      );
+      r.style.setProperty(
+        "--w-e-toolbar-active-color",
+        theme.palette.grey[500]
+      );
+      r.style.setProperty("--w-e-toolbar-active-color_options", "#000");
+    }
+  }, [theme.palette.mode]);
+
+  useEffect(() => {
+    return () => {
+      if (editor == null) return;
+      editor.destroy();
+      setEditor(null);
+    };
+  }, [editor]);
+
+  const toolbarConfig: Partial<IToolbarConfig> = {
+    excludeKeys: ["uploadVideo", "fullScreen"],
+    ...toolbarConfigProps,
+  };
+
+  const editorConfig: Partial<IEditorConfig> = {
+    placeholder: "Type here...",
+    MENU_CONF: {
+      fontFamily: {
+        fontFamilyList: [
+          "Arial",
+          "Tahoma",
+          "Verdana",
+          { name: "Tahoma", value: "Tahoma" },
+        ],
+      },
+      uploadImage: {
+        async customUpload(file: File, insertFn: Function) {
+          if (allData.onUpload) {
+            allData.onUpload(file as CustomFile).then((res) => {
+              insertFn(res.url, res.alt, res.href);
+            });
+          }
         },
       },
-      history: {
-        delay: 500,
-        maxStack: 100,
-        userOnly: true,
-      },
-      syntax: false,
-      clipboard: {
-        matchVisual: false,
-      },
-    }),
-    [id]
-  );
+    },
+    autoFocus: false,
+    onFocus: () => {
+      if (boxRef.current) {
+        boxRef.current.style.border = `solid 2px ${theme.palette.primary.main}`;
+      }
+    },
+    onBlur: () => {
+      if (boxRef.current) {
+        boxRef.current.style.border = `solid 1px ${
+          theme.palette.mode === "dark" ? "#ffffff3b" : "#0000003b"
+        }`;
+      }
+    },
+    ...editorConfigProps,
+  };
 
-  if (loading) {
+  if (loading || !load) {
     return (
       <Grid item {...allData.layoutProps}>
         <TextViewSkeleton height={253} hasHelper={!!helperText} />
@@ -84,42 +185,59 @@ const EditorView = <T extends FormikValues>(props: EditorViewProps<T>) => {
           {label}
         </InputLabel>
       )}
-      <EditorViewRootStyle
-        sx={(theme: Theme) => ({
-          ...(getData({ source: formik.touched, key: name }) &&
-            Boolean(getData({ source: formik.errors, key: name })) && {
-              border: `solid 1px ${theme.palette.error.main}`,
-            }),
-        })}
+      <Grid
+        ref={boxRef}
+        sx={{
+          border: `solid 1px ${
+            theme.palette.mode === "dark" ? "#ffffff3b" : "#0000003b"
+          }`,
+          ":hover": {
+            border: `solid 1px ${
+              theme.palette.mode === "dark" ? "#fff" : "#000"
+            }`,
+          },
+          borderRadius: "4px",
+          overflow: "hidden",
+        }}
+        onMouseEnter={() => {
+          if (boxRef.current && boxRef.current.style.borderWidth !== "2px") {
+            boxRef.current.style.border = `solid 1px ${
+              theme.palette.mode === "dark" ? "#fff" : "#000"
+            }`;
+          }
+        }}
+        onMouseLeave={() => {
+          if (boxRef.current && boxRef.current.style.borderWidth !== "2px") {
+            boxRef.current.style.border = `solid 1px ${
+              theme.palette.mode === "dark" ? "#ffffff3b" : "#0000003b"
+            }`;
+          }
+        }}
       >
-        <EditorToolbar
-          id={id}
-          className={`class-${id}`}
-          isSimple={allData.editorProps.isToolbarSimple}
-        />
-        {/* @ts-ignore */}
-        <ReactQuill
-          className={`class-${id}`}
-          value={getData({ source: formik.values, key: name })}
-          // value={editorValue}
-          modules={modules}
-          formats={formats}
-          onChange={(
-            value: string,
-            _delta: any,
-            _source: any,
-            editor: ReactQuill.UnprivilegedEditor
-          ) => {
-            formik.setFieldValue(
-              name,
-              editor.getLength() > 1 ? value : formik.initialValues[name]
-            );
+        <Toolbar
+          editor={editor}
+          defaultConfig={toolbarConfig}
+          mode="default"
+          style={{
+            borderBottom: `1px solid ${
+              theme.palette.mode === "dark" ? "#ffffff3b" : "#0000003b"
+            }`,
           }}
-          // onChange={(value) => setEditorValue(value)}
-          // onBlur={() => formik.setFieldValue(name, editorValue)}
-          {...editorProps}
+          {...toolbarComponentProps}
         />
-      </EditorViewRootStyle>
+        <Editor
+          defaultConfig={editorConfig}
+          value={getData({ source: formik.values, key: name })}
+          onCreated={setEditor}
+          onChange={(editorObj: any) => {
+            formik.setFieldValue(name, editorObj.getHtml());
+          }}
+          mode="default"
+          style={{ height: "300px" }}
+          {...editorComponentProps}
+        />
+      </Grid>
+
       <FormHelperText
         error={
           getData({ source: formik.touched, key: name }) &&

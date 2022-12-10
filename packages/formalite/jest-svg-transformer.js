6f7450e189ecc50303302e354623d22af69b2c8c
Mostafa Roosta <106796626+mostafaRoosta74@users.noreply.github.com>
2022-12-10 14:32:23 +0330

    fix: coverall action

--- packages/formalite/jest-svg-transformer.js
+++ packages/formalite/jest-svg-transformer.js
@@ -1,18 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  process(src, filePath) {
-    if (path.extname(filePath) !== ".svg") {
-      return src;
-    }
-
-    const name = `svg-${path.basename(filePath, ".svg")}`
-      .split(/\W+/)
-      .map((x) => `${x.charAt(0).toUpperCase()}${x.slice(1)}`)
-      .join("");
-
+  process(src, filename) {
     return {
-      code: `${name}`,
+      code: `module.exports =  ${JSON.stringify(path.basename(filename))};`,
     };
   },
 };
